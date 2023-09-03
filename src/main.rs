df5f026c250da8d8e8a8f65b2ef4d07df85048cd
dundargoc <gocdundar@gmail.com>
2023-09-03 16:38:43 +0200

    Add matrix sdk example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use matrix_sdk::{
+    Client, config::SyncSettings,
+    ruma::{user_id, events::room::message::SyncRoomMessageEvent},
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let alice = user_id!("@alice:example.org");
+    let client = Client::builder().user_id(alice).build().await?;
+
+    // First we need to log in.
+    client.login_username(alice, "password").send().await?;
+
+    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+        println!("Received a message {:?}", ev);
+    });
+
+    // Syncing is important to synchronize the client state with the server.
+    // This method will never return.
+    client.sync(SyncSettings::default()).await;
+
+    Ok(())
 }
